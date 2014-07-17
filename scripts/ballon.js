6d6d6c0d8cedf109cf80db586902f7ba6aaa54cb
Fernando Claverino <fernando.claverino@gmail.com>
2014-07-17 17:43:48 -0300

    add multiple ballons

--- scripts/ballon.js
+++ scripts/ballon.js
@@ -1,35 +1,35 @@
-var Ballon = function($game) {
-    var id = 'balloon1';
-    var $balloon = $('<img src="images/balloon1.png" class="balloon">');
-    var $boom = $('<img src="images/boom1.png" class="boom">');
+var Ballon = function(number, $game) {
+    var id = 'balloon' + number;
+    var $balloon = $('<img src="images/balloon' + number + '.png" class="balloon">');
+    var $boom = $('<img src="images/boom' + number + '.png" class="boom">');
     var $splash = $('<img src="images/splash_balloon.png" class="splash">');
+
     var $container = $('<div id="' + id + '"></div>');
     $container.append($balloon);
     $container.append($splash);
     $container.append($boom);
 
-
     var boom = function() {
         $balloon.hide();
-        
+
         $splash.fadeIn('fast');
 
         $boom.fadeIn('fast', function() {
-			move('#' + id + ' img.boom')
-        		.scale(1.2)
-        		.end(hide);
+            move('#' + id + ' img.boom')
+                .scale(1.2)
+                .end(hide);
         });
     };
 
     var hide = function() {
-    	$splash.fadeOut('fast');
-		$boom.fadeOut('slow');
+        $splash.fadeOut('fast');
+        $boom.fadeOut('slow');
     };
 
     var expand = function() {
         move('#' + id + ' img.balloon')
-        .scale(4)
-        .end(boom);
+            .scale(4)
+            .end(boom);
     };
 
     this.draw = function(x, y) {
@@ -39,4 +39,6 @@ var Ballon = function($game) {
         $balloon.click(expand);
         $game.append($container);
     };
+
+    return this;
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,10 @@
 var Game = function(gameElement) {
 	var $game = $(gameElement)
 	this.start = function() {
-		var ballon1 = new Ballon($game);
-		ballon1.draw(100,100);
+		var ballon1 = new Ballon(1, $game).draw(100,100);
+		var ballon1 = new Ballon(2, $game).draw(200,100);
+		var ballon1 = new Ballon(3, $game).draw(300,100);
+		var ballon1 = new Ballon(4, $game).draw(400,100);
+		var ballon1 = new Ballon(5, $game).draw(500,100);
 	};
 }
