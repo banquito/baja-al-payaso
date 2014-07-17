1b06c76026fa4fc57776f784956ee937b1778d2e
Fernando Claverino <fernando.claverino@gmail.com>
2014-07-17 16:19:11 -0300

    explote a single balloon

--- scripts/ballon.js
+++ scripts/ballon.js
@@ -9,36 +9,33 @@ var Ballon = function($game) {
     $container.append($boom);
 
 
-    var splash = function() {
-        $balloon.hide();
-        $splash.fadeIn('fast', boom);
-    }
-
     var boom = function() {
+        $balloon.hide();
+        
+        $splash.fadeIn('fast');
+
         $boom.fadeIn('fast', function() {
 			move('#' + id + ' img.boom')
-        		.scale(1.5)
+        		.scale(1.2)
         		.end(hide);
         });
     };
 
     var hide = function() {
-		//$splash.fadeOut('fast', function() {
-			$splash.fadeOut('fast');
-			$boom.fadeOut('fast');
-		//});
+    	$splash.fadeOut('fast');
+		$boom.fadeOut('slow');
     };
 
     var expand = function() {
         move('#' + id + ' img.balloon')
         .scale(4)
-        .end(splash);
+        .end(boom);
     };
 
     this.draw = function(x, y) {
         $container.css('left', x + 'px');
         $container.css('top', y + 'px');
-        $balloon.css('width', '10%');
+        $balloon.css('width', '18px');
         $balloon.click(expand);
         $game.append($container);
     };
