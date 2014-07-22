649a226e617f6ad8631236a308e91417ef10c77c
Fernando Claverino <fernando.claverino@gmail.com>
2014-07-21 21:43:08 -0300

    allow to down the clown

--- scripts/ballon.js
+++ scripts/ballon.js
@@ -1,14 +1,16 @@
-var Ballon = function(number, $game) {
+var Ballon = function(number, drownAnimation, $game) {
     var id = 'balloon' + number;
     var $number = $('#number' + number);
     var $balloon = $('#' + id + ' img.balloon');
     var $boom = $('#' + id + ' img.boom');
     var $splash = $('#' + id + ' img.splash');
     var $container = $('#' + id);
-    
+    var $drown = $('#splash' + number);
+
     var init = function() {
         $balloon.css('width', '18px');
-        $number.click(expand);
+        //$number.click(expand);
+        $number.click(drown);
     }
     
     var boom = function() {
@@ -32,6 +34,51 @@ var Ballon = function(number, $game) {
         move('#' + id + ' img.balloon')
             .scale(4)
             .end(boom);
+    };
+
+    var drown = function() {
+        move('#drown' + number)
+            .add(drownAnimation.property, drownAnimation.value)
+            .duration('1s')
+            .ease('in')
+            .then()
+                .skew(-10)
+                .duration('0.4s')
+                .ease('in')
+                .then()
+                    .skew(10)
+                    .duration('0.4s')
+                    .ease('in')
+                    .then()
+                        .skew(-10)
+                        .duration('0.4s')
+                        .ease('in')
+                        .then()
+                            .skew(10)
+                            .duration('0.4s')
+                            .ease('in')
+                            .then()
+                                .skew(-10)
+                                .duration('0.4s')
+                                .ease('in')
+                                .then()
+                                    .skew(10)
+                                    .duration('0.4s')
+                                    .ease('in')
+                                    .then()
+                                        .sub(drownAnimation.property, drownAnimation.value)
+                                        .duration('0.5s')
+                                        .ease('in')
+                                        .then(expand)
+                                    .pop()
+                                .pop()
+                            .pop()
+                        .pop()
+                    .pop()
+                .pop()
+                
+            .pop()
+            .end();
     };
 
     init();
--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,10 @@
 var Game = function(gameElement) {
 	var $game = $(gameElement)
 	this.start = function() {
-		var ballon1 = new Ballon(1, $game);
-		var ballon1 = new Ballon(2, $game);
-		var ballon1 = new Ballon(3, $game);
-		var ballon1 = new Ballon(4, $game);
-		var ballon1 = new Ballon(5, $game);
+		var ballon1 = new Ballon(1, { property: 'width', value: 110 }, $game);
+		var ballon1 = new Ballon(2, { property: 'width', value: 68 }, $game);
+		var ballon1 = new Ballon(3, { property: 'height', value: 58 }, $game);
+		var ballon1 = new Ballon(4, { property: 'width', value: 55 }, $game);
+		var ballon1 = new Ballon(5, { property: 'width', value: 110 }, $game);
 	};
 }
